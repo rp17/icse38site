d9a4bf87287d71e57dfeaa9d72eada9a548b70d6
rp17 <rp31@txstate.edu>
2015-01-12 15:34:38 -0600

    init video contest

--- icse38/icse38/urls.py
+++ icse38/icse38/urls.py
@@ -13,18 +13,28 @@ urlpatterns = patterns('',
     url(r'^base$', 'icse38.views.base', name='base'),
 
     url(r'^dates$', 'icse38.views.dates', name='base'),
+	
+	url(r'^videoContest$', 'icse38.views.videoContest', name='base'),
+	
+	url(r'^formsub$', 'icse38.views.formsub', name='base'),
 
     url(r'^team/organizing-committee$', 'icse38.views.oc', name='oc'),
 
-    url(r'^download/promo-banners$', 'icse38.views.promo', name='promo'),
+    url(r'^downloads$', 'icse38.views.downloads', name='downloads'),
 
     url(r'^venue$', 'icse38.views.venue', name='venue'),
+
+    url(r'^keynotes$', 'icse38.views.keynotes', name='keynotes'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    # url(r'^downloads/promo-banners$', 'icse38.views.promo', name='promo'),
+
+    # url(r'^downloads/benefactors-brochures$', 'icse38.views.brochures', name='brochures'),
 
 )
 
--- icse38/icse38/views.py
+++ icse38/icse38/views.py
@@ -15,11 +15,26 @@ def base(request):
 def dates(request):
     return render_to_response("dates.html", {}, RequestContext(request))
 
+def videoContest(request):
+    return render_to_response("videoContest.html", {}, RequestContext(request))
+	
+def formsub(request):
+    return render_to_response("formsub.html", {}, RequestContext(request))
+	
 def oc(request):
     return render_to_response("oc.html", {}, RequestContext(request))
 
-def promo(request):
-    return render_to_response("promo.html", {}, RequestContext(request))
+def downloads(request):
+    return render_to_response("downloads.html", {}, RequestContext(request))
 
 def venue(request):
     return render_to_response("venue.html", {}, RequestContext(request))
+
+def keynotes(request):
+    return render_to_response("keynotes.html", {}, RequestContext(request))
+
+# def brochures(request):
+#     return render_to_response("brochures.html", {}, RequestContext(request))
+
+# def promo(request):
+#     return render_to_response("promo.html", {}, RequestContext(request))
